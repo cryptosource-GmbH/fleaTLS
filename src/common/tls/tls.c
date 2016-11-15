f6dded390578f72c4ab086c449e1cfa785d6c3d8
Johannes Roth <johannes.roth@cryptosource.de>
2016-11-15 17:35:34 +0100

    ServerHello parsing working

--- src/common/tls/tls.c
+++ src/common/tls/tls.c
@@ -10,7 +10,7 @@
 
 #include <sys/types.h>
 #include <sys/socket.h>
-#include<arpa/inet.h> //inet_addr
+#include <arpa/inet.h> //inet_addr
 
 /*
    enum {
@@ -41,22 +41,32 @@
 */
 
 typedef enum {
-	HANDSHAKE_TYPE_HELLO_REQUEST = 0, 
-	HANDSHAKE_TYPE_CLIENT_HELLO = 1 
-		/*server_hello(2),
-       certificate(11), server_key_exchange (12),
-       certificate_request(13), server_hello_done(14),
-       certificate_verify(15), client_key_exchange(16),
-       finished(20)
-       (255)*/
-   } HandshakeType;
+	HANDSHAKE_TYPE_HELLO_REQUEST = 0,
+	HANDSHAKE_TYPE_CLIENT_HELLO = 1,
+	HANDSHAKE_TYPE_SERVER_HELLO = 2,
+	HANDSHAKE_TYPE_NEW_SESSION_TICKET = 4,
+    HANDSHAKE_TYPE_CERTIFICATE = 11,
+	HANDSHAKE_TYPE_SERVER_KEY_EXCHANGE = 12,
+    HANDSHAKE_TYPE_CERTIFCATE_REQUEST = 13,
+	HANDSHAKE_TYPE_SERVER_HELLO_DONE = 14,
+    HANDSHAKE_TYPE_CERTIFICATE_VERIFY = 15,
+	HANDSHAKE_TYPE_CLIENT_KEY_EXCHANGE = 16,
+    HANDSHAKE_TYPE_FINISHED = 20
+} HandshakeType;
 
 
-typedef enum { 
-	 RECORD_TYPE_CHANGE_CIPHER_SPEC=20,
-	 RECORD_TYPE_ALERT=21,
-	 RECORD_TYPE_HANDSHAKE=22,
-	 RECORD_TYPE_APPLICATION_DATA=23
+typedef enum {
+	 CONTENT_TYPE_CHANGE_CIPHER_SPEC=20,
+	 CONTENT_TYPE_ALERT=21,
+	 CONTENT_TYPE_HANDSHAKE=22,
+	 CONTENT_TYPE_APPLICATION_DATA=23,
+	 CONTENT_TYPE_HEARTBEAT=24
+} ContentType;
+
+typedef enum {
+	 RECORD_TYPE_PLAINTEXT,
+	 RECORD_TYPE_CIPHERTEXT,
+	 RECORD_TYPE_COMPRESSED,
 } RecordType;
 
 typedef struct {
@@ -66,6 +76,7 @@ typedef struct {
 
 typedef struct {
 	RecordType record_type;
+	ContentType content_type;
 	ProtocolVersion version;
 	flea_u16_t length;
 	flea_u8_t *data;
@@ -77,29 +88,192 @@ typedef struct {
 	flea_u8_t random_bytes[28];
 } Random;
 
-typedef flea_u32_t SessionID;	// 32 bits
-
-typedef enum { 
-	NO_COMPRESSION=0, 
-	COMPRESSION=255 
+typedef enum {
+	NO_COMPRESSION=0,
+	COMPRESSION=255
 } CompressionMethod;
 
 // TODO: Extensions
 
+
+typedef struct {
+   HandshakeType type;
+   flea_u32_t length;	// actually 24 Bit type !!
+   flea_u8_t* data;
+} HandshakeMessage;
+
 typedef struct {
 	ProtocolVersion client_version;
 	Random random;
+	flea_u8_t* session_id;
+	flea_u8_t session_id_length;
+	flea_u8_t* cipher_suites;
+	flea_u16_t cipher_suites_length;
+	CompressionMethod* compression_methods;
+	flea_u8_t compression_methods_length;
+	flea_u8_t* extensions;	// 2^16 bytes
+} ClientHello;
+
+typedef struct {
+	ProtocolVersion server_version;
+	Random random;
+	flea_u8_t* session_id;
+	flea_u8_t session_id_length;
+	flea_u8_t cipher_suite;
+	CompressionMethod compression_method;
+	flea_u8_t compression_methods_length;
+	flea_u8_t* extensions;	// 2^16 bytes
+} ServerHello;
+
+
+
+
+/**
+	TODO: Expected to take exactly one record layer message. One TCP packet can contain several record layer messages. need to handle this.
+	TODO: Could also be fragmented!
+	=> leave this function as is but add a function that takes care of this before passing values on to this function
+*/
+void read_record_message(flea_u8_t* bytes, flea_u8_t length, Record* record, RecordType record_type) {
+
+
+	if (record_type == RECORD_TYPE_PLAINTEXT)
+	{
+		if (length < 5)
+		{
+			return;	// TODO: error handling
+		}
+
+		record->content_type = bytes[0];
+		// TODO: check content type. Maybe "expected_content_type"
+		/*if (record->content_type < CONTENT_TYPE_CHANGE_CIPHER_SPEC || record->content_type > CONTENT_TYPE_HEARTBEAT)
+		{
+			return; // TODO: error handling
+		}*/
+
+
+		record->version.major = bytes[1];
+		record->version.minor = bytes[2];
+		if (record->version.minor != 0x03 && record->version.major != 3)
+		{
+			return; // TODO: error handling
+		}
+
+		flea_u8_t *p = (flea_u8_t*) &record->length;
+		p[0] = bytes[4];
+		p[1] = bytes[3];
+		if (record->length != length - 5)
+		{
+			return;	// TODO: error handling
+		}
+
+		// set length to length of the content
+		length = length-5;
+
+		// everything else should be plaintext data from now on
+		record->data = calloc(length, sizeof(flea_u8_t));
+		memcpy(record->data, bytes+5, sizeof(flea_u8_t)*length);
+	}
+}
+
+void read_handshake_message(Record* record, HandshakeMessage* handshake_msg, HandshakeType expected_type) {
+	if (record->length < 4)
+	{
+		return; // TODO: error handling
+	}
+
+	handshake_msg->type = record->data[0];
+	if (handshake_msg->type != expected_type) {
+		return; // TODO: error handling
+	}
+
+	flea_u8_t *p = (flea_u8_t*)&handshake_msg->length;
+	p[2] = record->data[1];
+	p[1] = record->data[2];
+	p[0] = record->data[3];
+
+	if (handshake_msg->length < record->length-4) {
+		return; // TODO: error handling
+	}
+
+	if (handshake_msg->length > record->length-4) {
+		// TODO: indicate somehow that this record is missing X byte and the handshake message is continuing in the next record
+	}
+	handshake_msg->data = calloc(record->length-4, sizeof(flea_u8_t));
+	memcpy(handshake_msg->data, record->data+4, sizeof(flea_u8_t)*(record->length-4));
+}
+
+/*
+typedef struct {
+	ProtocolVersion server_version;
+	Random random;
 	SessionID session_id;
 	flea_u8_t* cipher_suites;
-	flea_u16_t num_cipher_suites;
-	CompressionMethod compression_methods;
-	/*select (extensions_present) {
-	  case false:
-		  struct {};
-	  case true:
-		  Extension extensions<0..2^16-1>;
-	};*/
-} ClientHello;
+	flea_u16_t cipher_suites_length;
+	CompressionMethod compression_method;
+	flea_u8_t* extensions;	// 2^16 bytes
+} ServerHello;
+*/
+void read_server_hello(HandshakeMessage* handshake_msg, ServerHello* server_hello_msg)
+{
+	if (handshake_msg->length < 41) // min ServerHello length
+	{
+		return; // TODO: error handling
+	}
+
+	// keep track of length
+	int length = 0;
+
+	// read version
+	server_hello_msg->server_version.major = handshake_msg->data[length++];
+	server_hello_msg->server_version.minor = handshake_msg->data[length++];
+	if (server_hello_msg->server_version.major != 0x03 || server_hello_msg->server_version.minor != 0x03)
+	{
+		return; // TODO: error handling
+	}
+
+	// read random
+	flea_u8_t* p = (flea_u8_t*)&server_hello_msg->random.gmt_unix_time;
+	for (flea_u8_t i=0; i<4; i++)
+	{
+		p[i] = handshake_msg->data[length++];
+	}
+
+	p = server_hello_msg->random.random_bytes;
+	for (flea_u8_t i=0; i<28; i++)
+	{
+		p[i] = handshake_msg->data[length++];
+	}
+
+	// read session id length
+	server_hello_msg->session_id_length = handshake_msg->data[length++];
+	if (server_hello_msg->session_id_length > 0)
+	{
+		server_hello_msg->session_id = calloc(server_hello_msg->session_id_length, sizeof(flea_u8_t));
+		p = (flea_u8_t*)server_hello_msg->session_id;
+		for (flea_u8_t i=0; i<server_hello_msg->session_id_length; i++)
+		{
+			p[i] = handshake_msg->data[length++];
+		}
+		// TODO: NEED TO CHECK IF LENGTH STILL LONG ENOUGH
+	}
+
+	if (length + 3 > handshake_msg->length)
+	{
+		return; // TODO: error handling
+	}
+
+	// read cipher suites
+	p = (flea_u8_t*)&server_hello_msg->cipher_suite;
+	p[0] = handshake_msg->data[length++];
+	p[1] = handshake_msg->data[length++];
+
+	// read compression method
+	server_hello_msg->compression_method = handshake_msg->data[length++];
+
+	// TODO: parse extension
+	// for now simply ignore them
+}
+
 
 /**
 Variable-length vectors are defined by specifying a subrange of legal
@@ -111,9 +285,9 @@ void client_hello_to_bytes(ClientHello hello, flea_u8_t* bytes, flea_u16_t* leng
 {
 	flea_u16_t i=0;
 
-	memcpy(bytes, &hello.client_version.minor, sizeof(flea_u8_t));
+	memcpy(bytes, &hello.client_version.major, sizeof(flea_u8_t));
 	i += sizeof(flea_u8_t);
-	memcpy(bytes+i, &hello.client_version.major, sizeof(flea_u8_t));
+	memcpy(bytes+i, &hello.client_version.minor, sizeof(flea_u8_t));
 	i += sizeof(flea_u8_t);
 
 	memcpy(bytes+i, &hello.random.gmt_unix_time, sizeof(flea_u32_t));
@@ -121,11 +295,19 @@ void client_hello_to_bytes(ClientHello hello, flea_u8_t* bytes, flea_u16_t* leng
 	memcpy(bytes+i, hello.random.random_bytes, 28);
 	i += 28;
 
-	if (hello.session_id != 0)
+	int session_id_greater_0 = 0;
+	for (flea_u8_t j=0; i<32; j++)
 	{
-		bytes[i++] = sizeof(SessionID);
-		memcpy(bytes+i, hello.session_id, sizeof(SessionID));
-		i += sizeof(SessionID);
+		if (hello.session_id[j] != 0)
+		{
+			session_id_greater_0 = 1;
+		}
+	}
+	if (session_id_greater_0)
+	{
+		bytes[i++] = 32;
+		memcpy(bytes+i, hello.session_id, 32);
+		i += 32;
 	}
 	else
 	{
@@ -133,47 +315,63 @@ void client_hello_to_bytes(ClientHello hello, flea_u8_t* bytes, flea_u16_t* leng
 	}
 
 	// cipher suites length
-	flea_u16_t len_ciphersuites = hello.num_cipher_suites * 2; 
-	flea_u8_t *p = (flea_u8_t*) &len_ciphersuites;
+	flea_u8_t *p = (flea_u8_t*) &hello.cipher_suites_length;
 	bytes[i++] = p[1];
 	bytes[i++] = p[0];
 
-	for (flea_u8_t j=0; j<hello.num_cipher_suites; j++)
+	for (flea_u8_t j=0; j<hello.cipher_suites_length/2; j++)
 	{
 		bytes[i++] = hello.cipher_suites[2*j];
 		bytes[i++] = hello.cipher_suites[2*j+1];
 	}
 
-	bytes[i++] = 1;
-	bytes[i++] = hello.compression_methods;
+	bytes[i++] = hello.compression_methods_length;
+	for (flea_u8_t j=0; j<hello.compression_methods_length;j++) {
+		bytes[i++] = hello.compression_methods[j];
+	}
 
 	*length = i;
 }
 
-void create_handshake_message(HandshakeType type, flea_u8_t *in, flea_u32_t length_in, flea_u8_t *out, flea_u32_t *length_out) 
+void create_handshake_message(HandshakeType type, flea_u8_t *in, flea_u32_t length_in, flea_u8_t *out, flea_u32_t *length_out)
 {
 	flea_u8_t i=0;
 
+	// set handshake type
 	out[i++] = type;
-	
+
+	// set handshake length
 	flea_u8_t *p = (flea_u8_t*)&length_in;
 	out[i++] = p[2];
 	out[i++] = p[1];
 	out[i++] = p[0];
 
+	// copy all data
 	memcpy(out+i, in, length_in);
 	i += length_in;
 
 	*length_out = i;
 }
 
+flea_u32_t get_size_of_first_record(flea_u8_t* bytes, flea_u8_t length) {
+	if (length < 5) {
+		return; // TODO: error handling
+	}
+	flea_u16_t size;
+	flea_u8_t *p = (flea_u8_t*) &size;
+	p[0] = bytes[4];
+	p[1] = bytes[3];
+
+	return size+5;
+}
+
 void record_to_bytes(Record record, flea_u8_t *bytes, flea_u8_t *length)
 {
 	flea_u16_t i=0;
-	bytes[i++] = record.record_type;
+	bytes[i++] = record.content_type;
 	bytes[i++] = record.version.major;
 	bytes[i++] = record.version.minor;
-	
+
 	if (record.length <= 256)
 	{
 		bytes[i++] = 0;
@@ -185,15 +383,15 @@ void record_to_bytes(Record record, flea_u8_t *bytes, flea_u8_t *length)
 		memcpy(bytes+i, &record.length, 2);
 		i += 2;
 	}
-	
+
 	memcpy(bytes+i, record.data, record.length);
-	i += record.length;	
+	i += record.length;
 
 	*length = i;
 }
 
 
-void print_hello(ClientHello hello) 
+void print_client_hello(ClientHello hello)
 {
 	printf("\nPrinting Hello Struct\n");
 	printf("Protocol Version major, minor: %i, %i\n", hello.client_version.major, hello.client_version.minor);
@@ -205,15 +403,48 @@ void print_hello(ClientHello hello)
 	{
 		printf("%02x ", hello.random.random_bytes[i]);
 	}
-	printf("\nSessionID: %u\n", hello.session_id);
-	printf("Cipher Suites: ");
-	for (int i=0; i<hello.num_cipher_suites; i+=2)
+	printf("\nSessionID: \n");
+	for (flea_u8_t i=0; i<hello.session_id_length; i++)
+	{
+		printf("%02x ", hello.session_id[i]);
+	}
+
+	printf("\nCipher Suites: ");
+	for (flea_u8_t i=0; i<hello.cipher_suites_length/2; i+=2)
 	{
 		printf("(%02x, %02x) ", hello.cipher_suites[i], hello.cipher_suites[i+1]);
 	}
-	printf("\nCompression Method: %i\n\n", hello.compression_methods);
-	
-	
+
+	printf("\nCompression Methods: ");
+	for (flea_u8_t i=0; i<hello.compression_methods_length; i++)
+	{
+		printf("%02x ", hello.compression_methods[i]);
+	}
+}
+
+void print_server_hello(ServerHello hello)
+{
+	printf("\nPrinting Hello Struct\n");
+	printf("Protocol Version major, minor: %i, %i\n", hello.server_version.major, hello.server_version.minor);
+
+	printf("Random: \n");
+	printf("\tUnix Time %i", hello.random.gmt_unix_time);
+	printf("\n\trandom bytes ");
+	for (int i=0; i<28; i++)
+	{
+		printf("%02x ", hello.random.random_bytes[i]);
+	}
+	printf("\nSessionID: \n");
+	for (flea_u8_t i=0; i<hello.session_id_length; i++)
+	{
+		printf("%02x ", hello.session_id[i]);
+	}
+	printf("\nCipher Suite: ");
+	flea_u8_t* p = (flea_u8_t*)&hello.cipher_suite;
+	printf("(%02x, %02x) ", p[0], p[1]);
+
+	printf("\nCompression Method: ");
+	printf("%02x ", hello.compression_method);
 }
 
 ClientHello create_hello_message()	{
@@ -222,7 +453,7 @@ ClientHello create_hello_message()	{
 	flea_u8_t gmt_unix_time[4] = {0x00, 0x01, 0x02, 0x03};
 	flea_u8_t TLS_RSA_WITH_AES_256_CBC_SHA256[] = { 0x00, 0x3D };
 	flea_u8_t* cipher_suites = malloc(2);	// TODO deallocate
-	
+
 	ClientHello hello;
 	memset(&hello, 0, sizeof(ClientHello));
 
@@ -231,23 +462,25 @@ ClientHello create_hello_message()	{
 
 	// session ID empty => no resumption. TODO is 4 zero bytes == empty?
 	hello.session_id = 0;
-	
+
 	memcpy(&hello.random.gmt_unix_time, gmt_unix_time, sizeof(gmt_unix_time));
 	memcpy(hello.random.random_bytes, random_bytes, sizeof(random_bytes));
 
-	memcpy(cipher_suites, TLS_RSA_WITH_AES_256_CBC_SHA256, sizeof(TLS_RSA_WITH_AES_256_CBC_SHA256));	
+	memcpy(cipher_suites, TLS_RSA_WITH_AES_256_CBC_SHA256, sizeof(TLS_RSA_WITH_AES_256_CBC_SHA256));
 	hello.cipher_suites = cipher_suites;
-	hello.num_cipher_suites = 1;
-		
-	hello.compression_methods = NO_COMPRESSION;
+	hello.cipher_suites_length = 2;
 
-	return hello;	
+	hello.compression_methods = calloc(1, sizeof(flea_u8_t));
+	hello.compression_methods_length = 1;
+	hello.compression_methods[0] = NO_COMPRESSION;
+
+	return hello;
 }
 
 int create_socket() {
 	int socket_fd;
     socket_fd = socket(AF_INET , SOCK_STREAM , 0);
-     
+
     if (socket_fd == -1)
     {
         printf("Could not create socket");
@@ -261,7 +494,7 @@ int flea_tls_handshake(int socket_fd)
 	flea_u8_t reply[16384];
 
 	ClientHello hello = create_hello_message();
-	print_hello(hello);
+	print_client_hello(hello);
 
 	flea_u8_t hello_message[16384];
 	flea_u16_t length;
@@ -272,7 +505,7 @@ int flea_tls_handshake(int socket_fd)
 		printf("%02x ", hello_message[i]);
 	}
 	printf("\n\n");
-	
+
 	flea_u8_t handshake_message[16384];
 	flea_u32_t handshake_length;	// 24 bit
 	create_handshake_message(HANDSHAKE_TYPE_CLIENT_HELLO, hello_message, length, handshake_message, &handshake_length);
@@ -285,7 +518,8 @@ int flea_tls_handshake(int socket_fd)
 
 
 	Record hello_record;
-	hello_record.record_type = RECORD_TYPE_HANDSHAKE;
+	hello_record.content_type = CONTENT_TYPE_HANDSHAKE;
+	hello_record.record_type = RECORD_TYPE_PLAINTEXT;
 	hello_record.version.major = 3;
 	hello_record.version.minor = 3;
 	hello_record.length = handshake_length;
@@ -301,7 +535,7 @@ int flea_tls_handshake(int socket_fd)
 		printf("%02x ", record_message[i]);
 	}
 	printf("\n\n");
-	
+
 
 	printf("sending HelloClient ...\n");
 
@@ -327,21 +561,33 @@ int flea_tls_handshake(int socket_fd)
 		printf("send failed\n");
 
 	printf("receiving ...\n");
-	
+
 	flea_u32_t recv_bytes;
-	while(1)
-	{	
+	//while(1)
+	//{
 		recv_bytes = recv(socket_fd, reply, 16384, 0);
 		if (recv_bytes < 0)
 			printf("recv failed\n");
-	
+
 		printf("received message of length %i:\n", recv_bytes);
 		for (flea_u16_t i=0; i<recv_bytes; i++)
 		{
 			printf("%02x ", reply[i]);
 		}
 		printf("\n");
-	}
+
+		printf("Parsing ServerHello Message:\n");
+		Record server_hello_record;
+		HandshakeMessage handshake_message_server_hello;
+		ServerHello server_hello_message;
+
+		flea_u32_t first_record_size = get_size_of_first_record(reply, recv_bytes);
+
+		read_record_message(reply, first_record_size, &server_hello_record, RECORD_TYPE_PLAINTEXT);
+		read_handshake_message(&server_hello_record, &handshake_message_server_hello, HANDSHAKE_TYPE_SERVER_HELLO);
+		read_server_hello(&handshake_message_server_hello, &server_hello_message);
+		print_server_hello(server_hello_message);
+	//}
 }
 
 
@@ -352,20 +598,23 @@ int flea_tls_connection()
     struct sockaddr_in addr;
 
 	socket_fd = create_socket();
-    
+
 	memset(&addr, 0, sizeof(addr));
-    addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    /*addr.sin_addr.s_addr = inet_addr("127.0.0.1");
     addr.sin_family = AF_INET;
-    addr.sin_port = htons( 4444 );
- 
+    addr.sin_port = htons( 4444 );*/
+    addr.sin_addr.s_addr = inet_addr("31.15.64.162");
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons( 443 );
+
     if (connect(socket_fd , (struct sockaddr *)&addr , sizeof(addr)) < 0)
     {
         printf("connect error\n");
         return 1;
     }
 
-	flea_tls_handshake(socket_fd);     
-	
+	flea_tls_handshake(socket_fd);
+
     printf("Connected\n");
 	close (socket_fd);
     return 0;
