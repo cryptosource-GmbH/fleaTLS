f8677a8af0c6940742369671a6c411531b5e18a2
Falko Strenzke <fstrenzke@cryptosource.de>
2017-01-16 10:52:53 +0100

    a little cleanup

--- test/src/common/test_cert_verify.c
+++ test/src/common/test_cert_verify.c
@@ -18,7 +18,7 @@ flea_err_t THR_flea_test_cert_verify_rsa()
 {
 
   FLEA_THR_BEG_FUNC();
-#if defined FLEA_USE_STACK_BUF && (FLEA_RSA_MAX_KEY_BIT_SIZE >= 4096)
+#if FLEA_RSA_MAX_KEY_BIT_SIZE >= 4096
   FLEA_CCALL(THR_flea_x509_verify_cert_signature(test_cert_tls_server_1, sizeof(test_cert_tls_server_1), flea_test_cert_issuer_of_tls_server_1__cau8, sizeof(flea_test_cert_issuer_of_tls_server_1__cau8)));
 #endif
   FLEA_THR_FIN_SEC_empty();
@@ -30,7 +30,7 @@ flea_err_t THR_flea_test_cert_verify_rsa()
 flea_err_t THR_flea_test_cert_verify_ecdsa()
 {
   FLEA_THR_BEG_FUNC();
-#if defined FLEA_HAVE_ECDSA && FLEA_ECC_MAX_MOD_BIT_SIZE >= 384
+#if FLEA_ECC_MAX_MOD_BIT_SIZE >= 384
   FLEA_CCALL(THR_flea_x509_verify_cert_signature(test_self_signed_ecdsa_csca_cert__acu8, sizeof(test_self_signed_ecdsa_csca_cert__acu8), test_self_signed_ecdsa_csca_cert__acu8, sizeof(test_self_signed_ecdsa_csca_cert__acu8)));
 #endif
   FLEA_THR_FIN_SEC_empty();
