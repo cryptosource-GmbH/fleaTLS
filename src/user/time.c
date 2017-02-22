20c463b1e3ad95a20dbec08ea454ceac4a3ecef4
Falko Strenzke <fstrenzke@cryptosource.de>
2017-02-22 13:27:40 +0100

    fixed function name

--- src/user/time.c
+++ src/user/time.c
@@ -5,7 +5,7 @@
 #include "flea/error_handling.h"
 
 #ifdef FLEA_USE_USER_CURR_TIME
-flea_err_t flea_pltfif_time__get_current_time(flea_gmt_time_t* time__t)
+flea_err_t THR_flea_pltfif_time__get_current_time(flea_gmt_time_t* time__t)
 {
   FLEA_THR_BEG_FUNC();
 # error flea_pltfif_time__get_current_time needs to be implemented
