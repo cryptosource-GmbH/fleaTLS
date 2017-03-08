90047a60d6ae67001f767c5bb9411dac961a436b
Falko Strenzke <fstrenzke@cryptosource.de>
2017-03-08 11:45:38 +0100

    some corrections in makefile regarding debug build and correction of hard coded time for lpc1768

--- pltf_support/workaround_lpc/src/time.c
+++ pltf_support/workaround_lpc/src/time.c
@@ -8,9 +8,9 @@
 flea_err_t THR_flea_pltfif_time__get_current_time(flea_gmt_time_t* time__t)
 {
   FLEA_THR_BEG_FUNC();
-  time__t->year    = 2017;
+  time__t->year    = 2018;
   time__t->month   = 3;
-  time__t->day     = 3;
+  time__t->day     = 16;
   time__t->hours   = 0;
   time__t->seconds = 0;
   time__t->minutes = 0;
