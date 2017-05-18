094875bc19d5c3daf5e97eb919924389388f5d1e
Johannes Roth <johannes.roth@cryptosource.de>
2017-05-18 10:51:53 +0200

    fix endianess error (2)

--- src/common/tls/tls_common.c
+++ src/common/tls/tls_common.c
@@ -421,7 +421,7 @@ flea_err_t THR_flea_tls__send_certificate(
 {
   flea_u32_t hdr_len__u32;
   flea_u32_t cert_list_len__u32;
-  flea_u8_t cert_list_len_enc__au8[3];
+  flea_u8_t enc_len__au8[3];
 
   FLEA_THR_BEG_FUNC();
 
@@ -445,16 +445,16 @@ flea_err_t THR_flea_tls__send_certificate(
   );
 
 
-  cert_list_len__u32        = hdr_len__u32 - 3;
-  cert_list_len_enc__au8[0] = cert_list_len__u32 >> 16;
-  cert_list_len_enc__au8[1] = cert_list_len__u32 >> 8;
-  cert_list_len_enc__au8[2] = cert_list_len__u32;
+  cert_list_len__u32 = hdr_len__u32 - 3;
+  enc_len__au8[0]    = cert_list_len__u32 >> 16;
+  enc_len__au8[1]    = cert_list_len__u32 >> 8;
+  enc_len__au8[2]    = cert_list_len__u32;
 
   FLEA_CCALL(
     THR_flea_tls__send_handshake_message_content(
       &tls_ctx->rec_prot__t,
       hash_ctx,
-      cert_list_len_enc__au8,
+      enc_len__au8,
       3
     )
   );
@@ -462,28 +462,16 @@ flea_err_t THR_flea_tls__send_certificate(
   // TODO use stream function for encoding
   for(flea_u8_t i = 0; i < cert_chain_len__u8; i++)
   {
+    enc_len__au8[0] = cert_chain__pt[i].len__dtl >> 16;
+    enc_len__au8[1] = cert_chain__pt[i].len__dtl >> 8;
+    enc_len__au8[2] = cert_chain__pt[i].len__dtl;
+
     FLEA_CCALL(
       THR_flea_tls__send_handshake_message_content(
         &tls_ctx->rec_prot__t,
         hash_ctx,
-        &((flea_u8_t*) &(cert_chain__pt[i].len__dtl))[2],
-        1
-      )
-    );
-    FLEA_CCALL(
-      THR_flea_tls__send_handshake_message_content(
-        &tls_ctx->rec_prot__t,
-        hash_ctx,
-        &((flea_u8_t*) &(cert_chain__pt[i].len__dtl))[1],
-        1
-      )
-    );
-    FLEA_CCALL(
-      THR_flea_tls__send_handshake_message_content(
-        &tls_ctx->rec_prot__t,
-        hash_ctx,
-        &((flea_u8_t*) &(cert_chain__pt[i].len__dtl))[0],
-        1
+        enc_len__au8,
+        3
       )
     );
     FLEA_CCALL(
