a0493ca3ab0133c25f0a64bc8260f85b6e91871c
Falko Strenzke <fstrenzke@cryptosource.de>
2017-05-26 16:52:44 +0200

    corrected spot of argument diversifiction

--- include/api/flea/crl.h
+++ include/api/flea/crl.h
@@ -13,7 +13,6 @@ extern "C" {
 
 typedef enum { flea_revstat_undetermined, flea_revstat_revoked, flea_revstat_good } flea_revocation_status_e;
 
-// flea_err_t THR_flea_crl__check_revocation_status(const flea_x509_cert_ref_t *subject__pt, const flea_x509_cert_ref_t *issuer__pt, const flea_byte_vec_t *crl_der__cprcu8, flea_al_u16_t nb_crls__alu16,  const flea_gmt_time_t *verification_date__pt, flea_bool_t is_ca_cert__b, const flea_byte_vec_t *inherited_params_mbn__cprcu8);
 flea_err_t THR_flea_crl__check_revocation_status(
   const flea_x509_cert_ref_t* subject__pt,
   const flea_x509_cert_ref_t* issuer__pt,
@@ -23,18 +22,19 @@ flea_err_t THR_flea_crl__check_revocation_status(
   flea_bool_t                 is_ca_cert__b
 );
 
-flea_err_t THR_flea_crl__check_revocation_status_crl_stream(
-  const flea_x509_cert_ref_t* subject__pt,
-  const flea_x509_cert_ref_t* issuer__pt,
-  const flea_byte_vec_t*      crl_der__cprcu8,
-  flea_al_u16_t               nb_crls__alu16,
-  const flea_gmt_time_t*      verification_date__pt,
-  flea_bool_t                 is_ca_cert__b,
-  const flea_byte_vec_t*      subjects_issuer_dn_raw__pt,
-  const flea_byte_vec_t*      subjects_sn__pt,
-  const flea_byte_vec_t*      subjects_crldp_raw__pt,
-  const flea_public_key_t*    issuers_public_key__pt
-);
+/*
+ * flea_err_t THR_flea_crl__check_revocation_status_crl_stream(
+ * const flea_x509_cert_ref_t* subject__pt,
+ * const flea_x509_cert_ref_t* issuer__pt,
+ * const flea_byte_vec_t*      crl_der__cprcu8,
+ * flea_al_u16_t               nb_crls__alu16,
+ * const flea_gmt_time_t*      verification_date__pt,
+ * flea_bool_t                 is_ca_cert__b,
+ * const flea_byte_vec_t*      subjects_issuer_dn_raw__pt,
+ * const flea_byte_vec_t*      subjects_sn__pt,
+ * const flea_byte_vec_t*      subjects_crldp_raw__pt,
+ * const flea_public_key_t*    issuers_public_key__pt
+ * );*/
 #ifdef __cplusplus
 }
 #endif
