4b41d2443b5d6cb114b1625cecf2138cd3051d16
Falko Strenzke <fstrenzke@cryptosource.de>
2017-06-03 16:32:35 +0200

    added missing file

---
+++ include/internal/common/tls/tls_client_int.h
@@ -0,0 +1,19 @@
+/* ##__FLEA_LICENSE_TEXT_PLACEHOLDER__## */
+
+#ifndef _flea_tls_client_int__H_
+#define _flea_tls_client_int__H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+flea_err_t THR_flea_tls_ctx_t__client_handle_server_initiated_reneg(
+  flea_tls_ctx_t* tls_ctx__pt
+);
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* h-guard */
