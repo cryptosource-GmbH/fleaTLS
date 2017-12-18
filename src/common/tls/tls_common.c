54e010f4213298fe83fafc4fb7d9896f1a4e66e3
Falko Strenzke <fstrenzke@cryptosource.de>
2017-12-18 15:08:52 +0100

    added KU crl sign error -> alert

--- src/common/tls/tls_common.c
+++ src/common/tls/tls_common.c
@@ -97,6 +97,7 @@ static const error_alert_pair_t error_alert_map__act [] = {
   {FLEA_ERR_INV_MAC,                            FLEA_TLS_ALERT_DESC_BAD_RECORD_MAC     },
   {FLEA_ERR_STREAM_EOF,                         FLEA_TLS_ALERT_DESC_DECODE_ERROR       },
   {FLEA_ERR_POINT_NOT_ON_CURVE,                 FLEA_TLS_ALERT_DESC_ILLEGAL_PARAMETER  },
+  {FLEA_ERR_X509_CRL_ISSUER_WO_CRL_SIGN,        FLEA_TLS_ALERT_DESC_BAD_CERTIFICATE    }
 };
 
 static flea_bool_t determine_alert_from_error(
