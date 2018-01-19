b302633bc2c7c4aefc5a87ac549829b8b380a249
Johannes Roth <jroth@cryptosource.de>
2018-01-19 10:02:01 +0100

    fix test openssl_test descriptions and scripts for new build_cfg names

--- misc/tools/openssl_tests/build_cfg_variations_tests/cl_rsa4096_ecc_stack/run_client.sh
+++ misc/tools/openssl_tests/build_cfg_variations_tests/cl_rsa4096_ecc_stack/run_client.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-valgrind --leak-check=full ./build/test --tls_client --trusted=../tls_test_tool/misc/test_data/cpt_tests/tls_client_and_server/KEYSIZES_RSA_4096_valid/KEYSIZES_RSA_4096_ROOT_CA.TA.crt --port=4444 --ip_addr=127.0.0.1 --hostname=localhost --allowed_sig_algs=SHA1-RSA,SHA224-RSA,SHA256-RSA --no_hostn_ver --reneg_mode=only_secure_reneg --rev_chk=none
+valgrind --leak-check=full ./build/flea-test --tls_client --trusted=../tls_test_tool/misc/test_data/cpt_tests/tls_client_and_server/KEYSIZES_RSA_4096_valid/KEYSIZES_RSA_4096_ROOT_CA.TA.crt --port=4444 --ip_addr=127.0.0.1 --hostname=localhost --allowed_sig_algs=SHA1-RSA,SHA224-RSA,SHA256-RSA --no_hostn_ver --reneg_mode=only_secure_reneg --rev_chk=none
--- misc/tools/openssl_tests/rsa_key_size_cfg_too_low/run_server_2.sh
+++ misc/tools/openssl_tests/rsa_key_size_cfg_too_low/run_server_2.sh
@@ -1,2 +1,2 @@
-valgrind --leak-check=full ./build/unit_test --tls_server --own_certs=../tls_test_tool/misc/test_data/rsa_default/server.der --own_private_key=../tls_test_tool/misc/test_data/rsa_default/server.pkcs8 --port=4444 --ip_addr=127.0.0.1 --hostname=localhost --allowed_sig_algs=SHA1-RSA,SHA224-RSA,SHA256-RSA,SHA384-RSA,SHA512-RSA --no_hostn_ver --reneg_mode=only_secure_reneg --rev_chk=none
+valgrind --leak-check=full ./build/flea-test --tls_server --own_certs=../tls_test_tool/misc/test_data/rsa_default/server.der --own_private_key=../tls_test_tool/misc/test_data/rsa_default/server.pkcs8 --port=4444 --ip_addr=127.0.0.1 --hostname=localhost --allowed_sig_algs=SHA1-RSA,SHA224-RSA,SHA256-RSA,SHA384-RSA,SHA512-RSA --no_hostn_ver --reneg_mode=only_secure_reneg --rev_chk=none
 
