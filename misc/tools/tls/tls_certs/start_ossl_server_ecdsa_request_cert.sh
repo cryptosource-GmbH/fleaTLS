d9334b395258fb49ca50ab7a233c4d38c0e53c47
Johannes Roth <jroth@cryptosource.de>
2018-01-29 14:31:32 +0100

    Add scripts to run ECDSA with client auth. ECDSA working for client side

---
+++ misc/tools/tls/tls_certs/start_ossl_server_ecdsa_request_cert.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# change working directory to the script's path
+BASEDIR=$(dirname "$0")
+cd $BASEDIR
+
+openssl s_server -accept 4444 -CAfile rootCA_ecdsa.pem -key server_ecdsa.key -cert server_ecdsa.pem -Verify 5
