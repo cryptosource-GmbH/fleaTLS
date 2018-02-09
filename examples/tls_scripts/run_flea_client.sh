ee3adc6b59ff5137c5539e9afa629504f1b6b602
Johannes Roth <jroth@cryptosource.de>
2018-02-09 15:26:44 +0100

    more senseful allowed sig alg order in example scripts

--- examples/tls_scripts/run_flea_client.sh
+++ examples/tls_scripts/run_flea_client.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./build/flea-test --tls_client --trusted=./misc/testdata/KEYSIZES_RSA_4096/KEYSIZES_RSA_4096_ROOT_CA.TA.crt --port=4444 --ip_addr=127.0.0.1 --hostname=localhost --allowed_sig_algs=SHA1-ECDSA,SHA256-ECDSA,SHA1-RSA,SHA224-RSA,SHA256-RSA --no_hostn_ver --reneg_mode=only_secure_reneg --rev_chk=none
+./build/flea-test --tls_client --trusted=./misc/testdata/KEYSIZES_RSA_4096/KEYSIZES_RSA_4096_ROOT_CA.TA.crt --port=4444 --ip_addr=127.0.0.1 --hostname=localhost --allowed_sig_algs=SHA256-ECDSA,SHA256-RSA,SHA1-RSA,SHA224-RSA,SHA1-ECDSA --no_hostn_ver --reneg_mode=only_secure_reneg --rev_chk=none
