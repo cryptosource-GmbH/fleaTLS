b20f9fd3cdffe100acbd618687b7f13f0af71eb8
Falko Strenzke <fstrenzke@cryptosource.de>
2018-02-19 15:48:38 +0100

    fixed bugs oid resoving for named curves

--- src/common/ecc_named_curves.c
+++ src/common/ecc_named_curves.c
@@ -8,7 +8,7 @@
 const flea_u8_t brainpool_version_one_oid_prefix[] = {0x2B, 0x24, 0x03, 0x03, 0x02, 0x08, 0x01, 0x01};
 
 const flea_u8_t nist_secp_curve_prefix[] = {0x2B, 0x81, 0x04, 0x00};
-const flea_u8_t P256_oid[] = {0x2A, 0x86, 0x48, 0xCE, 0x3D, 0x01, 0x07};
+const flea_u8_t P256_oid[] = {0x2A, 0x86, 0x48, 0xCE, 0x3D, 0x03, 0x01, 0x07};
 
 flea_err_e THR_flea_ecc_gfp_dom_par_t__set_by_named_curve_oid(
   flea_ec_dom_par_ref_t* dp_to_set__pt,
@@ -38,7 +38,8 @@ flea_err_e THR_flea_ecc_gfp_dom_par_t__set_by_named_curve_oid(
     last_byte /= 2;
     result_id  = (flea_ec_dom_par_id_e) (flea_brainpoolP160r1 + last_byte);
   }
-  else if(oid_len__alu8 == sizeof(nist_secp_curve_prefix) + 1)
+  else if((oid_len__alu8 == sizeof(nist_secp_curve_prefix) + 1) &&
+    !memcmp(oid__pcu8, nist_secp_curve_prefix, sizeof(nist_secp_curve_prefix)))
   {
     /*secp224r1 OBJECT IDENTIFIER ::= { iso(1) identified-organization(3) certicom(132) curve(0) 33 }
      * secp384r1 OBJECT IDENTIFIER ::= { iso(1) identified-organization(3) certicom(132) curve(0) 34 }
@@ -59,7 +60,7 @@ flea_err_e THR_flea_ecc_gfp_dom_par_t__set_by_named_curve_oid(
           FLEA_THROW("invalid/unkonwn named curve OID", FLEA_ERR_ECC_INV_BUILTIN_DP_ID);
     }
   }
-  else if(flea_memcmp_wsize(P256_oid, sizeof(P256_oid), oid__pcu8, oid_len__alu8))
+  else if(!flea_memcmp_wsize(P256_oid, sizeof(P256_oid), oid__pcu8, oid_len__alu8))
   {
     /*     secp256r1 OBJECT IDENTIFIER ::= {
      *     iso(1) member-body(2) us(840) ansi-X9-62(10045) curves(3) prime(1) 7 }*/
