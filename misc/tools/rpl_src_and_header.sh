df26f40686078c356052e00201d7ed4e8a82cc1f
Falko Strenzke <fstrenzke@cryptosource.de>
2018-06-16 08:34:58 +0200

    adapted API doc for function renaming

--- misc/tools/rpl_src_and_header.sh
+++ misc/tools/rpl_src_and_header.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-find test/ -name "*.c" | xargs -n1 rpl $1 $2
-find test/ -name "*.cpp" | xargs -n1 rpl $1 $2
-find test/ -name "*.h" | xargs -n1 rpl $1 $2
-find src/ -name "*.c" | xargs -n1 rpl $1 $2
-find include/ -name "*.h" | xargs -n1 rpl $1 $2
-find build_cfg/ -name "*.h" | xargs -n1 rpl $1 $2
-find examples/ -name "*.c" | xargs -n1 rpl $1 $2
-find examples/ -name "*.h" | xargs -n1 rpl $1 $2
+#find test/ -name "*.c" | xargs -n1 rpl $1 $2
+#find test/ -name "*.cpp" | xargs -n1 rpl $1 $2
+#find test/ -name "*.h" | xargs -n1 rpl $1 $2
+#find src/ -name "*.c" | xargs -n1 rpl $1 $2
+#find include/ -name "*.h" | xargs -n1 rpl $1 $2
+#find build_cfg/ -name "*.h" | xargs -n1 rpl $1 $2
+#find examples/ -name "*.c" | xargs -n1 rpl $1 $2
+#find examples/ -name "*.h" | xargs -n1 rpl $1 $2
+find misc/doc/api_doc/pages/ -name "*.h" | xargs -n1 rpl $1 $2
+#find misc/doc/api_doc/pages/ -name "*.dox" | xargs -n1 rpl $1 $2
 
