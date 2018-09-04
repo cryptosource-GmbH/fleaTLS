e8115bdfe9cb318e01d991778c7542a6cb7b3133
Falko Strenzke <fstrenzke@cryptosource.de>
2018-09-04 13:07:18 +0200

    fixed build error

--- include/internal/common/build_config_util.h
+++ include/internal/common/build_config_util.h
@@ -1,104 +1,105 @@
 /* ##__FLEA_LICENSE_TEXT_PLACEHOLDER__## */
 
+#include "internal/common/tls/tls_const.h"
 
 #ifndef _flea_build_config_util__H_
-#define _flea_build_config_util__H_
+# define _flea_build_config_util__H_
 
-#if defined FLEA_HAVE_ECDSA
-# define FLEA_DO_IF_HAVE_ECDSA(x) x
-#else
-# define FLEA_DO_IF_HAVE_ECDSA(x)
-#endif
+# if defined FLEA_HAVE_ECDSA
+#  define FLEA_DO_IF_HAVE_ECDSA(x) x
+# else
+#  define FLEA_DO_IF_HAVE_ECDSA(x)
+# endif
 
-#if defined FLEA_HAVE_TLS_CS_ECDSA || defined FLEA_HAVE_TLS_CS_ECDH || defined FLEA_HAVE_TLS_CS_ECDHE
-# define FLEA_HAVE_TLS_CS_ECC
-#endif
+# if defined FLEA_HAVE_TLS_CS_ECDSA || defined FLEA_HAVE_TLS_CS_ECDH || defined FLEA_HAVE_TLS_CS_ECDHE
+#  define FLEA_HAVE_TLS_CS_ECC
+# endif
 
-#if defined FLEA_HAVE_TLS_CS_ECC
-# define FLEA_DO_IF_HAVE_TLS_ECC(x) x
-#else
-# define FLEA_DO_IF_HAVE_TLS_ECC(x)
-#endif
+# if defined FLEA_HAVE_TLS_CS_ECC
+#  define FLEA_DO_IF_HAVE_TLS_ECC(x) x
+# else
+#  define FLEA_DO_IF_HAVE_TLS_ECC(x)
+# endif
 
-#ifdef FLEA_SCCM_USE_PUBKEY_INPUT_BASED_DELAY
-# define FLEA_DO_IF_USE_PUBKEY_INPUT_BASED_DELAY(x) x
-#else
-# define FLEA_DO_IF_USE_PUBKEY_INPUT_BASED_DELAY(x)
-#endif
+# ifdef FLEA_SCCM_USE_PUBKEY_INPUT_BASED_DELAY
+#  define FLEA_DO_IF_USE_PUBKEY_INPUT_BASED_DELAY(x) x
+# else
+#  define FLEA_DO_IF_USE_PUBKEY_INPUT_BASED_DELAY(x)
+# endif
 
-#if defined FLEA_HAVE_ECDSA || defined FLEA_HAVE_ECKA
-# define FLEA_HAVE_ECC
-#endif
+# if defined FLEA_HAVE_ECDSA || defined FLEA_HAVE_ECKA
+#  define FLEA_HAVE_ECC
+# endif
 
-#if defined FLEA_HAVE_ECC
-# define FLEA_DO_IF_HAVE_ECC(x) x
-#else
-# define FLEA_DO_IF_HAVE_ECC(x)
-#endif
+# if defined FLEA_HAVE_ECC
+#  define FLEA_DO_IF_HAVE_ECC(x) x
+# else
+#  define FLEA_DO_IF_HAVE_ECC(x)
+# endif
 
 
-#ifdef FLEA_HAVE_RSA
-# define FLEA_HAVE_PK_CS
-#endif
+# ifdef FLEA_HAVE_RSA
+#  define FLEA_HAVE_PK_CS
+# endif
 
-#if defined FLEA_HAVE_RSA || defined FLEA_HAVE_ECDSA
-# define FLEA_HAVE_ASYM_SIG
-#endif
+# if defined FLEA_HAVE_RSA || defined FLEA_HAVE_ECDSA
+#  define FLEA_HAVE_ASYM_SIG
+# endif
 
-#if defined FLEA_HAVE_RSA || defined FLEA_HAVE_ECDSA || defined FLEA_HAVE_ECKA
-# define FLEA_HAVE_ASYM_ALGS
-#endif
+# if defined FLEA_HAVE_RSA || defined FLEA_HAVE_ECDSA || defined FLEA_HAVE_ECKA
+#  define FLEA_HAVE_ASYM_ALGS
+# endif
 
-#ifndef FLEA_HEAP_MODE
-# define FLEA_STACK_MODE
-#endif
+# ifndef FLEA_HEAP_MODE
+#  define FLEA_STACK_MODE
+# endif
 
-#ifdef FLEA_HAVE_AES_BLOCK_DECR
-# define FLEA_DO_IF_HAVE_AES_BLOCK_DECR(x) x
-#else
-# define FLEA_DO_IF_HAVE_AES_BLOCK_DECR(x)
-#endif
+# ifdef FLEA_HAVE_AES_BLOCK_DECR
+#  define FLEA_DO_IF_HAVE_AES_BLOCK_DECR(x) x
+# else
+#  define FLEA_DO_IF_HAVE_AES_BLOCK_DECR(x)
+# endif
 
-#if FLEA_CRT_RSA_WINDOW_SIZE > 1
-# define FLEA_DO_IF_RSA_CRT_WINDOW_SIZE_GREATER_ONE(x) do {x} while(0)
-#else
-# define FLEA_DO_IF_RSA_CRT_WINDOW_SIZE_GREATER_ONE(x)
-#endif
+# if FLEA_CRT_RSA_WINDOW_SIZE > 1
+#  define FLEA_DO_IF_RSA_CRT_WINDOW_SIZE_GREATER_ONE(x) do {x} while(0)
+# else
+#  define FLEA_DO_IF_RSA_CRT_WINDOW_SIZE_GREATER_ONE(x)
+# endif
 
 // fixed 32 bit size difference between P and Q is supported
-#define FLEA_RSA_CRT_PQ_BIT_DIFF                 32
-#define FLEA_RSA_CRT_PQ_BYTE_DIFF                ((FLEA_RSA_CRT_PQ_BIT_DIFF + 7) / 8)
-#define FLEA_RSA_CRT_KEY_COMPONENT_MAX_BYTE_SIZE ((FLEA_RSA_MAX_KEY_BYTE_SIZE + 1) / 2 + FLEA_RSA_CRT_PQ_BYTE_DIFF)
-#define FLEA_RSA_MAX_KEY_BYTE_SIZE               ((FLEA_RSA_MAX_KEY_BIT_SIZE + 7) / 8)
+# define FLEA_RSA_CRT_PQ_BIT_DIFF                 32
+# define FLEA_RSA_CRT_PQ_BYTE_DIFF                ((FLEA_RSA_CRT_PQ_BIT_DIFF + 7) / 8)
+# define FLEA_RSA_CRT_KEY_COMPONENT_MAX_BYTE_SIZE ((FLEA_RSA_MAX_KEY_BYTE_SIZE + 1) / 2 + FLEA_RSA_CRT_PQ_BYTE_DIFF)
+# define FLEA_RSA_MAX_KEY_BYTE_SIZE               ((FLEA_RSA_MAX_KEY_BIT_SIZE + 7) / 8)
 
-#define FLEA_ECC_MAX_COFACTOR_BYTE_SIZE          ((FLEA_ECC_MAX_COFACTOR_BIT_SIZE + 7) / 8)
+# define FLEA_ECC_MAX_COFACTOR_BYTE_SIZE          ((FLEA_ECC_MAX_COFACTOR_BIT_SIZE + 7) / 8)
 
 /************ Begin MAC and AE ************/
 
-#ifdef FLEA_HAVE_EAX
-# ifndef FLEA_HAVE_CMAC
-#  error CMAC MUST BE CONFIGURED FOR EAX MODE
+# ifdef FLEA_HAVE_EAX
+#  ifndef FLEA_HAVE_CMAC
+#   error CMAC MUST BE CONFIGURED FOR EAX MODE
+#  endif
 # endif
-#endif
 
-#if defined FLEA_HAVE_EAX || defined FLEA_HAVE_GCM
-# define FLEA_HAVE_AE
-#endif
+# if defined FLEA_HAVE_EAX || defined FLEA_HAVE_GCM
+#  define FLEA_HAVE_AE
+# endif
 
-#if defined FLEA_HAVE_HMAC || defined FLEA_HAVE_CMAC
-# define FLEA_HAVE_MAC
-#endif
+# if defined FLEA_HAVE_HMAC || defined FLEA_HAVE_CMAC
+#  define FLEA_HAVE_MAC
+# endif
 
 /************ End MAC and AE ************/
 
 
 /************ Begin Check Ciphersuite Configuration ************/
 
-#ifdef FLEA_HAVE_TLS
-# if !defined FLEA_HAVE_TLS_CS_GCM && !defined FLEA_HAVE_TLS_CS_CBC
-#  error for TLS, either FLEA_HAVE_TLS_CS_GCM  or FLEA_HAVE_TLS_CS_CBC must be defined
+# ifdef FLEA_HAVE_TLS
+#  if !defined FLEA_HAVE_TLS_CS_GCM && !defined FLEA_HAVE_TLS_CS_CBC
+#   error for TLS, either FLEA_HAVE_TLS_CS_GCM  or FLEA_HAVE_TLS_CS_CBC must be defined
+#  endif
 # endif
-#endif
 
 /************ End Check Ciphersuite Configuration ************/
 
@@ -106,68 +107,68 @@
 /************ Begin Record Size Configuration ************/
 
 /* FLEA_TLS_RECORD_MAX_RECEIVE_ADD_DATA_SIZE: maximum amount of data (padding, mac, ...) that can be added upon the plaintext of a record */
-#if defined FLEA_HAVE_TLS_CS_CBC
-# define FLEA_TLS_RECORD_MAX_RECEIVE_ADD_DATA_SIZE (256 + 16 + 48) /* Padding + IV(AES) + MAC(SHA384) */
-# define FLEA_TLS_RECORD_MAX_SEND_ADD_DATA_SIZE    (256 + 16 + 48) /* Padding + IV(AES) + MAC(SHA384) */
-#else // GCM
-# define FLEA_TLS_RECORD_MAX_RECEIVE_ADD_DATA_SIZE (8 + 16) /* Explicit IV + GCM tag */
-# define FLEA_TLS_RECORD_MAX_SEND_ADD_DATA_SIZE    (8 + 16) /* Explicit IV + GCM tag */
-#endif // if defined FLEA_HAVE_TLS_CS_CBC
+# if defined FLEA_HAVE_TLS_CS_CBC
+#  define FLEA_TLS_RECORD_MAX_RECEIVE_ADD_DATA_SIZE (256 + 16 + 48) /* Padding + IV(AES) + MAC(SHA384) */
+#  define FLEA_TLS_RECORD_MAX_SEND_ADD_DATA_SIZE    (256 + 16 + 48) /* Padding + IV(AES) + MAC(SHA384) */
+# else // GCM
+#  define FLEA_TLS_RECORD_MAX_RECEIVE_ADD_DATA_SIZE (8 + 16) /* Explicit IV + GCM tag */
+#  define FLEA_TLS_RECORD_MAX_SEND_ADD_DATA_SIZE    (8 + 16) /* Explicit IV + GCM tag */
+# endif // if defined FLEA_HAVE_TLS_CS_CBC
 
 /** the maximal data content of a TLS record */
-#define FLEA_TLS_RECORD_MAX_RECEIVE_SIZE \
+# define FLEA_TLS_RECORD_MAX_RECEIVE_SIZE \
   (FLEA_TLS_RECORD_MAX_RECEIVE_PLAINTEXT_SIZE \
   + FLEA_TLS_RECORD_MAX_RECEIVE_ADD_DATA_SIZE)
 
 
-#define FLEA_TLS_RECORD_MAX_SEND_SIZE \
+# define FLEA_TLS_RECORD_MAX_SEND_SIZE \
   (FLEA_TLS_RECORD_MAX_SEND_PLAINTEXT_SIZE \
   + FLEA_TLS_RECORD_MAX_SEND_ADD_DATA_SIZE)
 
 
 // throw error if
-#if FLEA_TLS_RECORD_MAX_RECEIVE_PLAINTEXT_SIZE < 512 && defined FLEA_TLS_HAVE_MAX_FRAG_LEN_EXT
-# error \
+# if FLEA_TLS_RECORD_MAX_RECEIVE_PLAINTEXT_SIZE < 512 && defined FLEA_TLS_HAVE_MAX_FRAG_LEN_EXT
+#  error \
   for TLS. Please deactivate the FLEA_TLS_HAVE_MAX_FRAG_LEN_EXT flag when using a FLEA_TLS_RECORD_MAX_RECEIVE_PLAINTEXT_SIZE < 512
-#endif
+# endif
 
 /************ End Record Size Configuration ************/
 
 /************ Begin Calculate Maximum Key Block Size ************/
 
-#if defined FLEA_HAVE_TLS_CS_CBC
-# if defined FLEA_HAVE_SHA384_512
-#  define FLEA_TLS_MAX_KEY_BLOCK_SIZE (2 * (32 + 48)) // AES256 + SHA384 is possible
-# else
-#  define FLEA_TLS_MAX_KEY_BLOCK_SIZE (2 * (32 + 32)) // AES256 + SHA256
-# endif
-#else // GCM only case
+# if defined FLEA_HAVE_TLS_CS_CBC
+#  if defined FLEA_HAVE_SHA384_512
+#   define FLEA_TLS_MAX_KEY_BLOCK_SIZE (2 * (32 + 48)) // AES256 + SHA384 is possible
+#  else
+#   define FLEA_TLS_MAX_KEY_BLOCK_SIZE (2 * (32 + 32)) // AES256 + SHA256
+#  endif
+# else // GCM only case
 /* always smaller than CBC case */
-# define FLEA_TLS_MAX_KEY_BLOCK_SIZE (2 * (32 + FLEA_CONST_TLS_GCM_FIXED_IV_LEN)) // AES256 + salt/implicit nonce
-#endif // if defined FLEA_HAVE_TLS_CS_CBC
+#  define FLEA_TLS_MAX_KEY_BLOCK_SIZE (2 * (32 + FLEA_CONST_TLS_GCM_FIXED_IV_LEN)) // AES256 + salt/implicit nonce
+# endif // if defined FLEA_HAVE_TLS_CS_CBC
 
 /************ End Calculate Maximum Key Block Size ************/
-#ifdef FLEA_COMPILER_GCC
-# define FLEA_ATTRIB_UNUSED_RESULT __attribute__((warn_unused_result))
-#else
-# define FLEA_ATTRIB_UNUSED_RESULT
-#endif
+# ifdef FLEA_COMPILER_GCC
+#  define FLEA_ATTRIB_UNUSED_RESULT __attribute__((warn_unused_result))
+# else
+#  define FLEA_ATTRIB_UNUSED_RESULT
+# endif
 
 
 /*********** Begin TLS/DTLS ********************************/
 
-#ifdef FLEA_HAVE_DTLS
-# define FLEA_DO_IF_HAVE_DTLS(x) x
-#else
-# define FLEA_DO_IF_HAVE_DTLS(x)
-#endif
+# ifdef FLEA_HAVE_DTLS
+#  define FLEA_DO_IF_HAVE_DTLS(x) x
+# else
+#  define FLEA_DO_IF_HAVE_DTLS(x)
+# endif
 
 /*********** End TLS/DTLS ********************************/
 
-#ifdef FLEA_COMPILER_GCC
-# define FLEA_ATTRIB_UNUSED_RESULT __attribute__((warn_unused_result))
-#else
-# define FLEA_ATTRIB_UNUSED_RESULT
-#endif
+# ifdef FLEA_COMPILER_GCC
+#  define FLEA_ATTRIB_UNUSED_RESULT __attribute__((warn_unused_result))
+# else
+#  define FLEA_ATTRIB_UNUSED_RESULT
+# endif
 
 #endif /* h-guard */
--- src/common/tls/handsh_msg_dtls.c
+++ src/common/tls/handsh_msg_dtls.c
@@ -13,24 +13,6 @@
 
 #ifdef FLEA_HAVE_DTLS
 
-// TODO: DELETE
-# define FLEA_DTLS_FLIGHT_BUF_CCS_CODE 0xFF
-
-# define FLIGHT_BUF_AT_CURRENT_READ_POS(hs_ctx__pt) \
-  (&(hs_ctx__pt)->dtls_ctx__t.flight_buf__bu8[(hs_ctx__pt)-> \
-  dtls_ctx__t.flight_buf_read_pos__u32])
-
-
-/*static flea_u32_t flea_dtls_hndsh__get_flight_buf_rem_free_size(
-  flea_tls_handshake_ctx_t* hs_ctx__pt
-)
-{
-  return FLEA_DTLS_FLIGHT_BUF_SIZE - tls_ctx__pt->dtls_retransm_state__t.flight_buf_write_pos__u32;
-}*/
-
-
-// TODO: MOVE TO RETRANSM_STATE
-
 
 flea_err_e THR_flea_dtls_hdsh__snd_hands_msg_hdr(
   flea_tls_handshake_ctx_t* hs_ctx__pt,
@@ -41,9 +23,6 @@ flea_err_e THR_flea_dtls_hdsh__snd_hands_msg_hdr(
 {
   flea_tls_ctx_t* tls_ctx__pt = hs_ctx__pt->tls_ctx__pt;
   flea_u8_t hdr__au8[FLEA_DTLS_HANDSH_HDR_LEN];
-
-  /*flea_recprot_t* rec_prot__pt = &hs_ctx__pt->tls_ctx__pt->rec_prot__t;
-  flea_al_u16_t wrt_rcrd_free_len__alu16;*/
 
   FLEA_THR_BEG_FUNC();
 
@@ -89,7 +68,6 @@ flea_err_e THR_flea_dtls_hdsh__snd_hands_msg_hdr(
       )
     );
   }
-  // tls_ctx__pt->dtls_retransm_state__t.send_msg_seq__s16++; // this is done in the
   // flight_buffer_writing
   FLEA_THR_FIN_SEC_empty();
 } /* THR_flea_tls__snd_hands_msg_hdr */
